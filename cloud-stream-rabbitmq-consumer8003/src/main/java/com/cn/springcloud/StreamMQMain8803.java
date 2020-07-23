05afe391c44638874098462dbb8a240d41d23b53
Summer <2641541955@qq.com>
2020-07-23 16:53:06 +0800

    SpringCloud Stream消息驱动,解决不同消息中间件的替换使用问题

---
+++ cloud-stream-rabbitmq-consumer8003/src/main/java/com/cn/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.cn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dark
+ * @date 2020-07-23 14:39
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/cn/springcloud/service/IMessageProviderService.java
@@ -0,0 +1,9 @@
+package com.cn.springcloud.service;
+
+/**
+ * @author Dark
+ * @date 2020-07-23 10:13
+ */
+public interface IMessageProviderService {
+    public String send();
+}
