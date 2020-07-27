d1ef742acda99e83129fbee2a055d9a0cc7e6480
Summer <2641541955@qq.com>
2020-07-27 15:32:56 +0800

    Spring Cloud Sleuth分布式请求链路追踪

--- cloud-stream-rabbitmq-provider8801/src/main/java/com/cn/springcloud/controller/SendMessageController.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/cn/springcloud/controller/SendMessageController.java
@@ -1,7 +1,6 @@
 package com.cn.springcloud.controller;
 
 import com.cn.springcloud.service.IMessageProviderService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
